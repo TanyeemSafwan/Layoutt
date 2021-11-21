3e2b240564cbc285bc7c0056800483d9a972724a
NULL <tanyeemsafwan123@gmail.com>
2021-11-21 23:22:50 +0600

    final

---
+++ app/src/main/java/com/example/myapplication/layoutt/part4_2.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.layoutt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class part4_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part42);
+    }
+}
